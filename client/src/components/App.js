3525da9f3c746eefcb30192478b6c6e643f3d0c6
Operaismo <eloperaismo@protonmail.com>
2022-03-11 19:49:38 -0600

    App.js - added explanation on why anchor tags are bad with React Router Dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+/* Bad Navigation
+1.) Adding <a> (anchor) tag 
+2.) browser issues request to localhost:3000
+3.) development server response with index.html
+4.) browser receives index.html file, dumps old HTML file it was showing 
+(this includes all React/Redux state data
+5.) index.html lists js files in script tags; browser downloads/executes the scripts
+6.) app starts */
+
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return <div>
+      PageOne
+
+      BAD! Don't do the below!
+      <br />
+      <a href="/pagetwo">Page 2</a>
+      </div>;
 };
 
 const PageTwo = () => {
-  return <div>PageTwo</div>;
+  return <div>
+      PageTwo
+      
+      BAD! Don't do the below!
+      <br />
+      <a href="/">Page 1</a>
+      </div>;
 };
 
 // React Router uses extractedPath.contains(path) to determine
