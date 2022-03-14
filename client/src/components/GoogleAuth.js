9ec29c9c69328319756e7f0939f0710ac89bffcb
Operaismo <eloperaismo@protonmail.com>
2022-03-13 22:03:18 -0500

    GoogleAuth.js - initialized state

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "271062148777-s32fovlsh62ehkngor36r2hdl0f42ru8.apps.googleusercontent.com",
-        scope: 'email'
-      }).then(() => {
+      window.gapi.client
+        .init({
+          clientId:
+            "271062148777-s32fovlsh62ehkngor36r2hdl0f42ru8.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-      });
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
   render() {
