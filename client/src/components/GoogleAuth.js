db7cf43ce6a2ffd87928f1f07814f7ae18595f60
Operaismo <eloperaismo@protonmail.com>
2022-03-13 21:29:54 -0500

    GoogleAuth.js - defined componentDidMount method to use gapi load with clientId and scope of email

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
 
 class GoogleAuth extends React.Component {
-    render() {
-        return <div>Google Auth</div>;
-    }
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "271062148777-s32fovlsh62ehkngor36r2hdl0f42ru8.apps.googleusercontent.com",
+        scope: 'email'
+      });
+    });
+  }
+  render() {
+    return <div>Google Auth</div>;
+  }
 }
